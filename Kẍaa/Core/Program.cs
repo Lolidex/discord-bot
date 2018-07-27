148007807a005e19e82bb85822c9293f19a75a4d
Xwilarg <xwilarg@yahoo.fr>
2018-07-28 01:52:17 +0200

    Add empty bot

---
+++ Kẍaa/Core/Program.cs
@@ -0,0 +1,33 @@
+﻿using DSharpPlus;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Kẍaa.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().RunAsync().Wait();
+
+        private DiscordClient _client;
+
+        public Program()
+        {
+            _client = new DiscordClient(new DiscordConfiguration()
+            {
+                AutoReconnect = true,
+                EnableCompression = true,
+                LogLevel = LogLevel.Debug,
+                Token = File.ReadAllText("Keys/token.dat"),
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true
+            });
+        }
+
+        public async Task RunAsync()
+        {
+            await _client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
